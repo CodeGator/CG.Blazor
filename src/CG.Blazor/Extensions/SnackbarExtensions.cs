85848b1bd9e8431754cbe657efed7c3cdf04124b
Martin Cook <33993749+martycook@users.noreply.github.com>
2023-01-27 07:16:41 -0600

    bug fixes

--- src/CG.Blazor/Extensions/SnackbarExtensions.cs
+++ src/CG.Blazor/Extensions/SnackbarExtensions.cs
@@ -17,18 +17,18 @@ public static class DialogServiceExtensions
     /// This method shows an error message, formatted from the given exception.
     /// </summary>
     /// <param name="dialogService">The dialog service to use for the operation.</param>
-    /// <param name="ex">The exception to use for the operation.</param>
+    /// <param name="exception">The exception to use for the operation.</param>
     /// <param name="title">The title to use for the operation.</param>
     /// <returns>A task to perform the operation.</returns>
     public static async Task ShowErrorBox(
         this IDialogService dialogService,
-        Exception ex,
+        Exception exception,
         string title = "Something broke!"
         )
     {
         // Validate the parameters before attempting to use them.
         Guard.Instance().ThrowIfNull(dialogService, nameof(dialogService))
-            .ThrowIfNull(ex, nameof(ex));
+            .ThrowIfNull(exception, nameof(exception));
 
         // Should we supply a default title?
         if (string.IsNullOrWhiteSpace(title)) 
@@ -40,23 +40,43 @@ public static class DialogServiceExtensions
         var sb = new StringBuilder();
         sb.Append($"<b>{title}</b><br /><br />");
         sb.Append($"<ul>");
-        sb.Append($"<li>{ex.Message}</li>");
+        sb.Append($"<li>{exception.Message}</li>");
 
         // Does the base error contain data?
-        if (ex.GetBaseException().Data.Count > 0)
+        if (exception.GetBaseException().Data.Count > 0)
         {
             // Loop through the data.
-            foreach (var obj in ex.GetBaseException().Data)
+            foreach (var obj in exception.GetBaseException().Data)
             {
                 // Add each bit of data.
                 sb.Append($"<li>{obj}</li>");
-            }            
+            }
+
+            sb.Append($"</ul><br /><ul>");
         }
-        else
+
+        // Get the exception message.
+        var msg = exception.GetBaseException().Message;
+
+        // Look for delimiters in the message.
+        var parts = msg.Split('|');
+
+        // Did wee find any?
+        if (parts.Length > 1)
         {
-            // Otherwise, just add the message from the base error.
-            sb.Append($"<li>{ex.GetBaseException().Message}</li>");
+            // Loop through the parts.
+            foreach (var part in parts)
+            {
+                // Add each part.
+                sb.Append($"<li>{part}</li>");
+            }
         }
+        else 
+        {
+            // Otherwise, just add the message verbatim.
+            sb.Append($"<li>{msg}</li>");
+        }
+
         sb.Append($"</ul>");
 
         // Display the formatted error message.
