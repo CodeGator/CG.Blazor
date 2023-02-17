6f2cc5c2c9122bd1566a1f8165cc4b366c2a169b
Martin Cook <33993749+martycook@users.noreply.github.com>
2023-02-17 12:17:31 -0600

    force a build

---
+++ src/CG.Blazor/Components/DualList/DualList.razor.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CG.Blazor.Components;
+
+/// <summary>
+/// This class is the code-behind for the <see cref="DualList"/> control.
+/// </summary>
+public partial class DualList
+{
+
+}
